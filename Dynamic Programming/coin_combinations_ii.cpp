2ddbec3059d8b892db30724614391dde7a08b1e9
Mayank Kushwaha <mayankofficial205@gmail.com>
2025-10-27 00:33:56 +0530

    Day 8

---
+++ Dynamic Programming/coin_combinations_ii.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define fastio ios::sync_with_stdio(0); cin.tie(0);
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define rrep(i,a,b) for (int i = a; i >= b; i--)
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+int main() {
+    fastio
+
+    int n, x;
+    cin >> n >> x;
+
+    vi c(n);
+    rep(i, 0, n) cin >> c[i];
+    vll dp(x + 1, 0);
+    dp[0] = 1;
+    rep(i, 0, n) {
+        rep(j, c[i], x + 1) {
+            dp[j] = (dp[j] + dp[j - c[i]]) % MOD;
+        }
+    }
+    cout << dp[x] << endl;
+
+    // cout << output << endl;
+    // ans == output ? cout << "Success!" : cout << "Didn't match" << endl;
+    return 0;
+}
